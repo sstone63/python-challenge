fb6515e7df5dfeba1b4c7bb6ba17d142ae2d7328
sstone63 <35824970+sstone63@users.noreply.github.com>
2018-03-11 16:11:13 -0400

    Add files via upload

--- pybank/main.py
+++ pybank/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 #How can I organize my directories to make this step simpler?
-csv_path = os.path.join(os.path.dirname("pybank"), "budget_data_1.csv")
+csv_path = 'budget_data_1.csv'
 
 budget_data = pd.read_csv(csv_path)
 
@@ -10,20 +10,22 @@ Total_Months = budget_data["Date"].nunique()
 
 Total_Revenue = budget_data["Revenue"].sum()
 
-Average_Revenue_Change = budget_data["Revenue"].mean()
+Average_Revenue_Change = round(budget_data["Revenue"].mean())
 
 Greatest_Increase_R = budget_data["Revenue"].max()
 
 Greatest_Decrease_R = budget_data["Revenue"].min()
 
+Date_Max = budget_data.loc[budget_data["Revenue"]==Greatest_Increase_R, "Date"].item()
+
+Date_Min = budget_data.loc[budget_data["Revenue"]==Greatest_Decrease_R, "Date"].item()
+
 print("Financial Analysis") 
 print("-------------------")
 print("Total Months: " + str(Total_Months))
-print("Total Revenue: " + str(Total_Revenue))
-print("Average Reveneue Change: " + str(Average_Revenue_Change))
-print("Greatest Increase in Revenue: " + str(Greatest_Increase_R))
-print("Greatest Decrease in Revenue: " + str(Greatest_Decrease_R))
+print("Total Revenue: " + "$" + str(Total_Revenue))
+print("Average Reveneue Change: " + "$" + str(Average_Revenue_Change))
+print("Greatest Increase in Revenue: " + Date_Max + " " + "$" + str(Greatest_Increase_R))
+print("Greatest Decrease in Revenue: " + Date_Min + " " + "$" + str(Greatest_Decrease_R))
 
 budget_data.to_csv("new_budget_data.csv", index = False, header = True)
-
-       
